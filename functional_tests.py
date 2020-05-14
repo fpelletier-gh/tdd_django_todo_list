83e6bf36d30d185174877435d94c3a02da504a64
Francis Pelletier <francispelletier2@hotmail.com>
2020-05-13 21:47:10 -0400

    Functional test checks if we can input a todo item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import time
 import unittest
 
 
@@ -15,16 +18,27 @@ class NewVisitorTest(unittest.TestCase):
 
         # He look at the title and header and it mention to-do
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name("h1")
+        self.assertIn("To-Do", header_text)
         # He is invited to enter a todo item
+        inputbox = self.browser.find_element_by_id("new-item-input")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # He enter "Groceries" in an input
+        inputbox.send_keys("Groceries")
 
         # When he hit enter, the page updates and the page show a list
         # "1: Groceries" is displayed
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("list-table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(any(row.text == "1: Groceries" for row in rows))
 
         # There is still an input to enter a new item
         # He enter a new item "Wash the dishes"
+        self.fail("Finish the test!")
 
         # The page updates and show the updated list with both items
 
