7b8454dc17ae8d0f49a34880fd2d30d4ea08bb1a
Francis Pelletier <francispelletier2@hotmail.com>
2020-05-17 21:06:55 -0400

    Added a new url for new list

--- lists/urls.py
+++ lists/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "lists"
 urlpatterns = [
     path("", views.home_page, name="home_page"),
-    path("lists/unique-list", views.view_list, name="view_list"),
+    path("lists/new", views.new_list, name="new_list"),
+    path("lists/unique-list/", views.view_list, name="view_list"),
 ]
 
